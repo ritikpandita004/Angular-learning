3d4cae4a4a3665cf56ffb52fa40c9577865eae30
ritikpandita004 <ritikpandita04@gmail.com>
2024-08-12 16:22:56 +0530

    Angular learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RoomsComponent } from "./rooms/rooms.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RoomsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Angular-Learning';
+
+  data=2
 }
